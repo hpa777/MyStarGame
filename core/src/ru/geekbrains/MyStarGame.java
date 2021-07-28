db9dfb43bbf4a5d5ea114aad5a9753694659a473
Pavel A. Khanin <pkhanyn@gmail.com>
2021-07-28 17:15:45 +0300

    lesson 1 HW

--- core/src/ru/geekbrains/MyStarGame.java
+++ core/src/ru/geekbrains/MyStarGame.java
@@ -1,24 +1,29 @@
 package ru.geekbrains;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MyStarGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	Texture backgroundImage;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		backgroundImage = new Texture("star-wars-space-background.jpg");
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(0, 0, 0, 1);
 		batch.begin();
+		batch.draw(backgroundImage, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		batch.draw(img, 0, 0);
 		batch.end();
 	}
